56121cc0193a6ed4313a0f030336476555eda934
Szymon Zosgornik <szymon.zosgornik@gmail.com>
2021-08-10 23:19:59 +0200

    Add macos to CI, clean up config script and prepare for libcpp

--- ci/config_compiler.sh
+++ ci/config_compiler.sh
@@ -3,17 +3,13 @@ set -Eeuo pipefail
 
 case "$1" in
   *gcc*)
-    sudo apt install gcc-10 g++-10
-    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 10
-    echo "CC=/usr/bin/gcc" >> "$GITHUB_ENV"
-    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-10 10
-    echo "CXX=/usr/bin/g++" >> "$GITHUB_ENV"
+    echo "CC=$(which gcc-10)" >> "$GITHUB_ENV"
+    echo "CXX=$(which g++-10)" >> "$GITHUB_ENV"
     ;;
   *clang*)
-    sudo apt install llvm-12-dev clang-12 lld-12 libc++-12-dev libc++abi-12-dev
-    sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-12 10
-    echo "CC=/usr/bin/clang" >> "$GITHUB_ENV"
-    sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-12 10
-    echo "CXX=/usr/bin/clang++" >> "$GITHUB_ENV"
+    CC="$(which clang-12)" || CC="$(brew --prefix llvm)/bin/clang"
+    CXX="$(which clang++-12)" || CXX="$(brew --prefix llvm)/bin/clang++"
+    echo "CC=$CC" >> "$GITHUB_ENV"
+    echo "CXX=$CXX" >> "$GITHUB_ENV"
     ;;
 esac
