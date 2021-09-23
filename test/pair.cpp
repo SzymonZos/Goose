9c65f1b11a99672e3293cbbb9c4e90b697df9f02
Szymon Zosgornik <szymon.zosgornik@gmail.com>
2021-09-23 22:54:44 +0200

    Extend pair and vector test cases

--- test/pair.cpp
+++ test/pair.cpp
@@ -17,6 +17,13 @@ TEST_CASE("Empty pair of vectors stringification", "[Empty]") {
     REQUIRE(gos::to_string(pair) == "[[], []]");
 }
 
+TEST_CASE("Pair of pair stringification", "[Scalar]") {
+    using Pair = std::pair<std::pair<int, int>, std::pair<float, float>>;
+    Pair pair{{5425, -31659893}, {534.654745F, -0.653463657F}};
+    REQUIRE(gos::to_string(pair) ==
+            "[[5425, -31659893], [534.655, -0.653464]]");
+}
+
 TEST_CASE("Pair of vectors stringification", "[Scalar]") {
     using Vec = std::vector<int>;
     Vec vec{0, 10, 0, -1, 3, 2, 1};
--- test/vector.cpp
+++ test/vector.cpp
@@ -10,18 +10,34 @@ TEST_CASE("Empty vector stringification", "[Empty]") {
     REQUIRE(gos::to_string(vec) == "[]");
 }
 
-TEST_CASE("Vector basic stringification", "[Scalar]") {
+TEST_CASE("Empty 2D vector stringification", "[Empty]") {
+    std::vector<std::vector<int>> vec{};
+    REQUIRE(gos::to_string(vec) == "[]");
+}
+
+TEST_CASE("Empty 4D vector stringification", "[Empty]") {
+    std::vector<std::vector<std::vector<std::vector<int>>>> vec{};
+    REQUIRE(gos::to_string(vec) == "[]");
+}
+
+TEST_CASE("Int vector stringification", "[Scalar]") {
     std::vector<int> vec{0, 10, 0, -1, 3, 2, 1};
     REQUIRE(gos::to_string(vec) == "[0, 10, 0, -1, 3, 2, 1]");
 }
 
-TEST_CASE("Empty vector of vectors stringification", "[Empty]") {
-    using Vec2 = std::vector<std::vector<int>>;
-    Vec2 vec2{};
-    REQUIRE(gos::to_string(vec2) == "[]");
+TEST_CASE("Double vector stringification", "[Scalar]") {
+    std::vector<double> vec{0.3647438573,
+                            10.5348534,
+                            -53450.63565,
+                            -1.54364367609,
+                            343252355.767567,
+                            2.654675478,
+                            176575676.534};
+    REQUIRE(gos::to_string(vec) == "[0.364744, 10.5349, -53450.6, -1.54364, "
+                                   "3.43252e+08, 2.65468, 1.76576e+08]");
 }
 
-TEST_CASE("Vector of vectors of int stringification", "[Complex]") {
+TEST_CASE("2D int vector stringification", "[Complex]") {
     using Vec2 = std::vector<std::vector<int>>;
     std::vector<int> vec{0, 10, 0, -1, 3, 2, 1};
     Vec2 vec2{vec, vec, vec, vec};
@@ -29,5 +45,15 @@ TEST_CASE("Vector of vectors of int stringification", "[Complex]") {
                                     " [0, 10, 0, -1, 3, 2, 1],\n"
                                     " [0, 10, 0, -1, 3, 2, 1],\n"
                                     " [0, 10, 0, -1, 3, 2, 1]}");
+}
+
+TEST_CASE("3D int vector basic stringification", "[Complex]") {
+    using Vec3 = std::vector<std::vector<std::vector<int>>>;
+    Vec3 vec{{{2, -2159, 69504, 664}, {543, 654}, {9098, 543}},
+             {},
+             {{11, 23}, {777}}};
+    REQUIRE(gos::to_string(vec) ==
+            "{{[2, -2159, 69504, 664],\n [543, 654],\n "
+            "[9098, 543]},\n [],\n {[11, 23],\n [777]}}");
 }
 } // namespace
